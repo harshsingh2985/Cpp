6a0f80d47f891c159c2b955787fc4da203ae52c3
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-07-22 01:35:27 +0530

    update:question.cpp

--- arrays/pratisechef.cpp
+++ arrays/pratisechef.cpp
@@ -1,16 +1,29 @@
+// 4
+// 3
+// START38 LTIME108 START38
+// 4
+// LTIME108 LTIME108 LTIME108 START38
+// 2
+// LTIME108 LTIME108
+// 6
+// START38 LTIME108 LTIME108 LTIME108 START38 LTIME108
+// 2 1
+// 1 3
+// 0 2
+// 2 4
 #include<iostream>
 using namespace std;
 int main(){
     int t;
     cin>>t;
     while(t--){
-        // taking input in the arrays
         int n;
         cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-            
-        } 
+
+        int start38=0;
+        int ltime=0;
+        string str;
+        
     }
 }
+

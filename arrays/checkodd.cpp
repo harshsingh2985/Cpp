9ff951d84fe05d2c3c76a83facd7d7d5e405b902
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-08-02 15:35:58 +0530

    update:checkodd or even

--- arrays/checkodd.cpp
+++ arrays/checkodd.cpp
@@ -1,3 +1,5 @@
+// checking priting the odd number in the arrays or not
+
 #include<iostream>
 using namespace std;
 
@@ -11,7 +13,15 @@ int main(){
         int a[n];
         for(int i=0;i<n;i++){
             cin>>a[i];
+            int odd=0;
 
+            if(a%2==0){
+
+            }else{
+                odd++;
+
+            }
+            cout<<odd<<endl;
             
         }
     }

4ca23f90b8d1c8071b0b812afb9922c4fed79528
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-08-03 05:00:11 +0530

    frequency.cpp
    program to find the frequency of the given number in the arrays

---
+++ arrays/frequency.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i]; 
+    }
+
+    // 2.second step to calculate the maxiumum number in the arrays
+    int maxi=0;
+    for(int i=0;i<n;i++){
+        maxi=max(maxi,a[i]);
+    }
+    // 3.third initalise the frequecny arrays with i+1;
+    int frequecny[maxi+1]={0};
+
+    for(int i=0;i<n;i++){
+        frequecny[a[i]]++;
+
+    }
+    // 4.Priting the answers
+    for(int i=0;i<n;i++){
+        if(frequecny[i]!=0){
+            cout<<i<<"->"<<frequecny[i]<<endl;
+
+        }
+    }
+}

bc01d96c95d839feef56fe07e913b158f1a99c94
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-08-03 04:36:38 +0530

    update

---
+++ moduleoperator/base.cpp
@@ -0,0 +1,10 @@
+// Calculate the given factorial of the number using Module Operator
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
+}
